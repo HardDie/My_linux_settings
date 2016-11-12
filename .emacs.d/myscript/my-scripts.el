044bf353f8f34e9f639d656ff1e1bcfdbc09a393
Sysoev Oleg <oleg.sysoev@eltex.loc>
2016-11-12 13:11:13 +0700

    Add emacs function \"reformat-region\"

--- .emacs.d/myscript/my-scripts.el
+++ .emacs.d/myscript/my-scripts.el
@@ -5,3 +5,15 @@
   (kill-line (- 1 arg)))
 
 (provide 'my-scripts)
+
+(defun reformat-region (&optional b e)
+    (interactive "r")
+    (when (not (buffer-file-name))
+      (error "A buffer must be associated with a file in order to use REFORMAT-REGION."))
+    (when (not (executable-find "clang-format"))
+      (error "clang-format not found."))
+    (shell-command-on-region b e
+                                                "clang-format -style=Google"
+                                                (current-buffer) t)
+    (indent-region b e)
+    )
