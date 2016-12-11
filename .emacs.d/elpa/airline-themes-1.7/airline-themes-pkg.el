1779886b9931a180e46d9076b979bad88d4522e5
HardDie <oleg1995sysoev@yandex.ru>
2016-12-12 06:09:12 +0700

    Emacs. Add installed files powerline, zoom, airline. Remove archive files

---
+++ .emacs.d/elpa/airline-themes-1.7/airline-themes-pkg.el
@@ -0,0 +1,7 @@
+(define-package "airline-themes" "1.7" "vim-airline themes for emacs powerline"
+  '((powerline "2.3"))
+  :url "http://github.com/AnthonyDiGirolamo/airline-themes" :keywords
+  '("evil" "mode-line" "powerline" "airline" "themes"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .emacs.d/elpa/zoom-window-0.5/zoom-window-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "zoom-window" "0.5" "Zoom window like tmux" '((emacs "24.3")) :url "https://github.com/syohex/emacs-zoom-window")
