5dd3422a2250ddce5e5a8e08941805f76415087d
HardDie <oleg1995sysoev@yandex.ru>
2017-04-01 01:37:07 +0700

    TimeRecord. Now time_sum may sum mounth time

--- TimeRecording/time_sum.sh
+++ TimeRecording/time_sum.sh
@@ -3,10 +3,26 @@ if [[ $# -lt 2 ]]; then
     echo 'Too few argument';
     exit;
 fi;
+
 result_sum=0;
-for i in $@; do
-    StartDate=$(date -u -d "$result_sum" +"%s");
-    FinalDate=$(date -u -d "$i" +"%s");
-    result_sum=$(date -u -d "0 $StartDate sec + $FinalDate sec" +"%H:%M:%S");
-done
+FILE=~/Desktop/time.log
+
+if [[ -z `echo $1 | grep -v :` ]]; then
+    # If input time
+    source=$@;
+else
+    # If input year and month
+    last_year=$1;
+    last_month=$2;
+    source=`cat $FILE | grep -A 2 "$last_year" | grep -iA 2 "$last_month" | grep "up" | awk '$3 ~/hours/' | awk '{ print $2":"$4 }'`;
+    source=$source" "`cat $FILE | grep -A 2 "$last_year" | grep -iA 2 "$last_month" | grep "up" | awk '$3 ~/minutes/' | awk '{ print "00:"$2 }'`;
+fi;
+
+if [[ ! -z $source ]]; then
+    for i in $source; do
+        StartDate=$(date -u -d "$result_sum" +"%s");
+        FinalDate=$(date -u -d "$i" +"%s");
+        result_sum=$(date -u -d "0 $StartDate sec + $FinalDate sec" +"%H:%M:%S");
+    done
+fi;
 echo $result_sum;
