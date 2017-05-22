dc908bec2aa12608b9abc1b3f7d23a3530f31352
Oleg Sysoev <oleg.sysoev@eltex.loc>
2017-05-22 16:07:22 +0700

    Emacs. Disable time and modeline on powerline

--- .emacs.d/myscript/my-settings.el
+++ .emacs.d/myscript/my-settings.el
@@ -118,8 +118,8 @@
 (setq-default indicate-buffer-boundaries 'left) ;; индикация только слева
 
 ;; Display file size/time in mode-line
-(setq display-time-24hr-format t) ;; 24-часовой временной формат в mode-line
-(display-time-mode             t) ;; показывать часы в mode-line
+;(setq display-time-24hr-format t) ;; 24-часовой временной формат в mode-line
+;(display-time-mode             t) ;; показывать часы в mode-line
 (size-indication-mode          t) ;; размер файла в %-ах
 
 ;; Line wrapping
