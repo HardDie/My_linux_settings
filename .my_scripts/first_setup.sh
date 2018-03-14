ec2c493c8bf99f7c7ea844b42f9bea737dc966a6
Oleg Sysoev <oleg.sysoev@eltex.loc>
2018-03-14 19:01:50 +0700

    Scripts. Add setup multilib in pacman

--- .my_scripts/first_setup.sh
+++ .my_scripts/first_setup.sh
@@ -25,9 +25,13 @@ HOST_NAME="ICONIA"
 echo $HOST_NAME > /etc/hostname
 
 ## Setup color pacman
-sed -ie 's/^#Color/Color\nILoveCandy/' /etc/locale.gen
+sed -ie 's/^#Color/Color\nILoveCandy/' /etc/pacman.conf
+
+## Enable multilib in pacman
+sed -ie 's/^#\[multilib\]/\[multilib\]\nInclude = \/etc\/pacman.d\/mirrorlist/' /etc/pacman.conf
 
 ## yaourt
+echo >> /etc/pacman.conf
 echo '[archlinuxfr]' >> /etc/pacman.conf
 echo 'SigLevel = Never' >> /etc/pacman.conf
 echo 'Server = http://repo.archlinux.fr/$arch' >> /etc/pacman.conf
