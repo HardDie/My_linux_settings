1c27d93efa2deebcd53bc4a8c8f546825b17468f
Oleg Sysoev <oleg.sysoev@eltex.loc>
2018-07-02 10:21:14 +0700

    Emacs. Scripts. Rewrite debug_header from macros to functions

--- .my_scripts/.data/debug_header.h
+++ .my_scripts/.data/debug_header.h
@@ -1,81 +1,152 @@
 #ifndef _MY_DEBUG_HEADER_
 #define _MY_DEBUG_HEADER_
 
+/**
+ * Global information
+ */
+
+#define D_ARGS const char* func, int line, const char* file
+#define D_INFO __FUNCTION__, __LINE__, __FILE__
+
+#define L_ALL (L_ERROR | L_WARN | L_INFO | L_DEBUG | L_STRACE | L_EXTRA)
+typedef enum {
+	L_RAW = 0,      /**< Level: raw print */
+	L_ERROR = 8,    /**< Level: error */
+	L_WARN  = 16,   /**< Level: warrning */
+	L_INFO  = 32,   /**< Level: info */
+	L_DEBUG = 64,   /**< Level: debug */
+	L_STRACE = 128, /**< Level: stack trace */
+	L_EXTRA = 256,  /**< Level: print extra information */
+
+	S_TR_ENT = L_STRACE | 1,  /**< Stack trace enter */
+	S_TR_EXT = L_STRACE | 2,  /**< Stack trace out */
+	S_TR_ERR = L_STRACE | 3,  /**< Stack trace error out */
+} debug_level_t;
+
+/**
+ * Init for main file
+ */
+
+#ifdef __D_INIT__
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
 #include <time.h>
 #include <sys/time.h>
 
-/* DEBUG */
-#ifndef I_EXT
-int _i = 0;
-#else
-extern int _i;
-#endif
+#define ANSI_COLOR_RESET   "\x1B[0m"
+#define ANSI_COLOR_RED     "\x1B[31m"
+#define ANSI_COLOR_GREEN   "\x1B[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_MAGENTA "\x1B[35m"
+#define ANSI_COLOR_CYAN    "\x1B[36m"
 
-static int _in = 1;
-enum {_ERR=31,_ENT=32,_EXT=35,_C_OPT};
-static const char* _n(int i) {
-	if(i==_ERR)return"ERR";
-	if(i==_EXT)return"EXT";
-	if(i==_ENT)return"ENT";
-	return"NaN";
-}
+#define D_PASS_ARGS func, line, file
 
-//#define STDOUT
+static void print_information(D_ARGS);
+static void print_indent(const int opt);
+static const char* get_prefix(const int opt);
 
-static inline void _p(const char*ff,...) {
-	long int f=(__intptr_t)ff;
-#ifndef STDOUT
-	FILE*F=fopen("/dev/console","a");
-#else
-	FILE*F=stdout;
-#endif
-	if(F) {
+__attribute__((unused)) int  debug_enable = 1;
+__attribute__((unused)) int  debug_level = L_ALL;
+__attribute__((unused)) char debug_file[256] = "/dev/console";
+
+__attribute__((unused))
+void dtrace(D_ARGS, const int opt, const char* format, ...) {
+
+	if (debug_file[0] == 0 || !debug_enable) {
+		return;
+	}
+	if (opt != L_RAW) { // Skip if L_RAW
+		if (!(opt & debug_level)) {
+			return;
+		}
+		print_indent(opt);
+	}
+
+	FILE* F = fopen(debug_file, "a");
+	if (F) {
+		if (opt != L_RAW) { // Skip if L_RAW
+			fprintf(F, "%s", get_prefix(opt));
+		}
 		va_list a;
-		va_start(a,ff);
-		if(f<_C_OPT) {
-			fprintf(F,"\x1B[%ldm%s\x1B[0m:", f, _n(f));
-			ff=va_arg(a,char*);
-		}
-		vfprintf(F,ff,a);
-		va_end(a);
+		va_start(a, format);
+		vfprintf(F, format, a);
 		fflush(F);
-#ifndef STDOUT
+		va_end(a);
 		fclose(F);
-#endif
+	}
+	if (opt != L_RAW) { // Skip if L_RAW
+		print_information(D_PASS_ARGS);
 	}
 }
-
-static inline void _i_p(int m) {
-	int i;
-	if(m==_EXT||m==_ERR) {
-		if(_i>0) {
-			_i--;
-		}
+__attribute__((unused))
+void trace_set_file(const char* file_) {
+	if (!file_) {
+		return;
 	}
-	for(i=0;i<_i;i++) {
-		_p("%s|  %s","\x1B[36m","\x1B[0m");
+	snprintf(debug_file, sizeof(debug_file), "%s", file_);
+}
+__attribute__((unused))
+void trace_set_level(const int mode, const debug_level_t level) {
+	if (mode == 1) {
+		debug_level |= level;
 	}
-	if(m==_ENT) {
-		_i++;
+	if (mode == 0) {
+		debug_level &= ~level;
 	}
 }
-
-static inline void _p_i(const char* func, int line, int pid, const char* file) {
-	if (_in) {
+__attribute__((unused))
+static inline void print_information(D_ARGS) {
+	if (debug_level & L_EXTRA) {
 		struct timeval t;
 		char time[32];
 		gettimeofday(&t, NULL);
 		strftime(time, sizeof(time), "%H:%M:%S", localtime(&t.tv_sec));
-		_p(" %1s[%s():%d]{%d}(%s) - %s%1s","\x1B[33m",func,line,pid,time,file,"\x1B[0m");
+		dtrace(D_INFO, L_RAW, " %1s[%s():%d]{%d}(%s) - %s%1s", ANSI_COLOR_YELLOW,
+		       func, line, getpid(), time, file, ANSI_COLOR_RESET);
 	}
-	_p("\n");
+	dtrace(D_INFO, L_RAW, "\n");
+}
+__attribute__((unused))
+static inline void print_indent(const int opt) {
+	static int indent = 0;
+	int i;
+	if (!(debug_level & L_STRACE)) { // If stack trace disable, not print indent
+		return;
+	}
+	if(opt == S_TR_EXT || opt == S_TR_ERR) {
+		if(indent > 0) {
+			indent--;
+		}
+	}
+	for(i = 0; i < indent; i++) {
+		dtrace(D_INFO, L_RAW, ANSI_COLOR_CYAN "|  " ANSI_COLOR_RESET);
+	}
+	if(opt == S_TR_ENT) {
+		indent++;
+	}
+}
+__attribute__((unused))
+static inline const char* get_prefix(const int opt) {
+	if (opt==S_TR_ENT) return ANSI_COLOR_GREEN   "ENT"     ANSI_COLOR_RESET;
+	if (opt==S_TR_EXT) return ANSI_COLOR_MAGENTA "EXT"     ANSI_COLOR_RESET;
+	if (opt==S_TR_ERR) return ANSI_COLOR_RED     "ERR"     ANSI_COLOR_RESET;
+	if (opt==L_DEBUG)  return                    "DEBUG: ";
+	if (opt==L_INFO)   return                    "INFO: ";
+	if (opt==L_WARN)   return ANSI_COLOR_YELLOW  "WARN: "  ANSI_COLOR_RESET;
+	if (opt==L_ERROR)  return ANSI_COLOR_RED     "ERROR: " ANSI_COLOR_RESET;
+	return"NaN";
 }
 
-#define _pwi(_f,...) do{_i_p((__intptr_t)_f);_p((char*)_f,##__VA_ARGS__);}while(0)
-#define DPRINT(_fl,_f,...)do{if(_fl){_pwi((char*)_f,##__VA_ARGS__);_p_i(__FUNCTION__, __LINE__, getpid(), __FILE__);if(!_i)_p("\n");}}while(0)
-/* DEBUG */
+/**
+ * Header for additional files
+ */
+
+#else // __D_INIT__
+extern void dtrace(D_ARGS, const int opt, const char* format, ...);
+extern void trace_set_file(const char* file_);
+extern void trace_set_level(const int mode, const debug_level_t level);
+#endif // __D_INIT__
 
 #endif // _MY_DEBUG_HEADER_
