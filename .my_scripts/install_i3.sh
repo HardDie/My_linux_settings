a043fb7637eb30bf52084bb0a96958ac43549fec
Oleg Sysoev <oleg.sysoev@eltex.loc>
2018-12-29 00:44:31 +0700

    Scripts. Add tool for tmux clipboard

--- .my_scripts/install_i3.sh
+++ .my_scripts/install_i3.sh
@@ -57,6 +57,9 @@ PCKGS+=" polkit"
 PCKGS+=" htop"
 PCKGS+=" unzip"
 
+## For tmux copy shortcut
+PCKGS+=" xclip"
+
 ## Change layout
 sudo install -Dm 0644 .data/00-keyboard.conf /etc/X11/xorg.conf.d/00-keyboard.conf
 
