44fd5edb969c073549b66dd58fd3abe4f1ba9aae
HardDie <oleg1995sysoev@yandex.ru>
2019-08-16 09:44:44 +0300

    Sway. Fix script for start lockscreen for one screen

--- .my_scripts/.data/sway_start_lockscreen.sh
+++ .my_scripts/.data/sway_start_lockscreen.sh
@@ -20,7 +20,7 @@ passwordCorrect=00000000
 passwordIncorrect=d23c3dff
 background=00000000
 foreground=ffffffff
-swaylock -i HDMI-A-1:.wallpapers/lockscreen_l.png -i HDMI-A-2:.wallpapers/lockscreen_r.png \
+swaylock -i ~/.wallpapers/lockscreen.png \
 	--inside-color=$background --ring-color=$foreground --line-uses-inside \
 	--key-hl-color=$letterEnteredColor --bs-hl-color=$letterRemovedColor --separator-color=$background \
 	--inside-ver-color=$passwordCorrect --inside-wrong-color=$passwordIncorrect \
