12ded6618a8dcc8f75e7d9f1040f683b93cd102c
HardDie <oleg1995sysoev@yandex.ru>
2021-05-26 11:12:54 +0300

    Added script for creating links on config files

---
+++ makelinks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+stow --dotfiles -t "$HOME" vim
+stow --dotfiles -t "$HOME" zsh
+stow --dotfiles -t "$HOME" tmux
+stow --dotfiles -t "$HOME" git
+stow --dotfiles -t "$HOME" fonts
+stow --dotfiles -t "$HOME" ncmpcpp
+stow --dotfiles -t "$HOME" my_scripts
+
+stow --dotfiles -t "$HOME/.config" termite
+stow --dotfiles -t "$HOME/.config" i3
+stow --dotfiles -t "$HOME/.config" oh-my-zsh
+stow --dotfiles -t "$HOME/.config" dunst
+stow --dotfiles -t "$HOME/.config" sway
