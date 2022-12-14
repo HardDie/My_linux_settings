04a19e6dd1781c0f20384e5081e883fc1cbf913a
HardDie <oleg1995sysoev@yandex.ru>
2022-12-14 21:25:07 +0300

    Sway. Added check wget in new_wallpapers script

--- my_scripts/dot-my_scripts/new_wallpapers.sh
+++ my_scripts/dot-my_scripts/new_wallpapers.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 #https://source.unsplash.com/1920x1080/?
 
+check_bin() {
+	which $1 > /dev/null 2>/dev/null
+	RES=$?
+	if [[ $RES -eq 1 ]]; then
+		echo "Can't find '$1' bin, try install '$2' packet"
+		exit
+	fi
+}
+
+check_bin "wget" "wget"
+
 USERID=$(id -u)
 SOCK=$(ls /run/user/${USERID}/sway-ipc.*.sock)
 
