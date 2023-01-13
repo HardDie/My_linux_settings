dba970c38e611a0228df0ec42382fecc82b3dbe8
HardDie <oleg1995sysoev@yandex.ru>
2023-01-13 10:46:37 +0300

    Sway. Changed waybar run script

--- sway/sway/start_waybar.sh
+++ sway/sway/start_waybar.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-USERID=$(id -u)
-SOCK=$(ls /run/user/${USERID}/sway-ipc.*.sock)
-SWAYSOCK=${SOCK} waybar -c ~/.config/sway/waybar.conf -s ~/.config/sway/style.css
+set -e
+
+function sig()
+{
+	echo "Trap"
+}
+
+trap sig RTMIN+1
+trap sig RTMIN+2
+trap sig RTMIN+3
+
+while true; do
+	echo "$(date) Starting waybar..." >>/tmp/waybar.log
+	USERID=$(id -u)
+	SOCK=$(ls /run/user/${USERID}/sway-ipc.*.sock)
+	{
+		SWAYSOCK=${SOCK} waybar -c ~/.config/sway/waybar.conf -s ~/.config/sway/style.css $@ 2>>/tmp/waybar.log
+	} || {
+		echo "Got error $?"
+	}
+	echo "$(date) Waybar were closed with code: $?" >>/tmp/waybar.log
+done
